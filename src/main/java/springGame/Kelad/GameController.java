1a226b0ef57fdaebb322ff1cade0975b550e8985
Kory <kory232323@gmail.com>
2023-07-04 12:08:15 -0500

    getting shit done

--- src/main/java/springGame/Kelad/GameController.java
+++ src/main/java/springGame/Kelad/GameController.java
@@ -8,17 +8,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @CrossOrigin(origins = "http://localhost:3000")
-@RestController
+@RestController // annotation allowing SpringBoot to register this class
 @RequestMapping("/game")
+// used to forward incoming HTTP requests to the correct method in this GameController.Java file
 public class GameController {
 
     @GetMapping("/start")
     public ResponseEntity<List<Country>> startGame() {
-        List<Country> countries = IntStream.range(0, 10).mapToObj(i -> Country.generateRandom()).collect(Collectors.toList());
+//        List<Country> countries = IntStream.range(0, 10).mapToObj(i -> Country.generateRandom()).collect(Collectors.toList());
+
+        // Create an IntStream ranging from 0 to 10
+        IntStream intStream = IntStream.range(0, 10);
+
+        // lambda equivalent of below anon class
+        IntFunction<Country> generateRandomCountry = value -> Country.generateRandom();
+        // Create a new anonymous class of type IntFunction (a functional interface)
+        // that overrides the apply method to generate a random Country
+//        IntFunction<Country> generateRandomCountry = new IntFunction<Country>() {
+//
+//            @Override
+        // apply() is part of IntFunction and is automatically called when we call mapToObj on this object.
+        // it's also called without being explicitly defined in the lambda equivalent above.
+//            public Country apply(int value) {
+//                return Country.generateRandom();
+//            }
+//        };
+
+        // Use mapToObj to apply the generateRandomCountry function to each element in the IntStream
+        /*
+            We create a Stream that takes a generic type, which we decide is our self-defined Country class. We then call the static method mapToObj with our IntFunction<Country> generateRandomCountry. Underneath the hood, the apply function is being called allowing the generateRandom() function to be called. Thus, 10 random countries are created with random names.
+         */
+        Stream<Country> countryStream = intStream.mapToObj(generateRandomCountry);
+
+        // Collect the resulting Stream into a List
+        List<Country> countries = countryStream.collect(Collectors.toList());
+
         return new ResponseEntity<>(countries, HttpStatus.OK);
     }
 }
--- src/main/java/springGame/Kelad/KeladApplication.java
+++ src/main/java/springGame/Kelad/KeladApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*
+Has the following bundled in:
+@Configuration: Tags the class as a source of bean definitions for the application context.
+@EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings. It's like telling Spring Boot to guess and configure the beans you're likely to need.
+@ComponentScan: Tells Spring to look for other components, configurations, and services in the springGame.Kelad package, allowing it to find and register the GameController.
+
+ */
 public class KeladApplication {
 
 	public static void main(String[] args) {
